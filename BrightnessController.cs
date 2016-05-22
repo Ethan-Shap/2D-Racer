9657cd0b63b06a95a7e10f2e5c9a1c120867313e
Ethan-Shap <ethancodes@gmail.com>
2016-05-22 07:50:10 -0700

    Adding For Question
    StackOverflow Question scripts On Brightness

---
+++ BrightnessController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BrightnessController : MonoBehaviour {
+
+    private PlayerPrefsManager pPM;
+
+	// Use this for initialization
+	void Start () {
+        pPM = GameObject.FindObjectOfType<PlayerPrefsManager>();
+
+        pPM.SetBrightness(0);
+        ChangeBrightness(pPM.GetBrightness());
+    }
+
+    public void ChangeBrightness(int brightness)
+    {
+        Debug.Log(GetComponent<Image>().color);
+        Color c = GetComponent<Image>().color;
+        c.a = brightness;
+        GetComponent<Image>().color = c;
+    }
+
+}
